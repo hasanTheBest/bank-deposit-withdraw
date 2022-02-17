5162ce00bec50ffc34cc4280853194d94cf99b6e
Mahmud <rainbowmahmud@gmail.com>
2022-02-17 19:08:04 +0600

    redirect to banking page after log in on github fixed.

--- js/login.js
+++ js/login.js
@@ -17,11 +17,15 @@ document.getElementById("login-submit").addEventListener("click", (e) => {
 });
 
 function redirectToBanking(email, password) {
-  // const url = window.location.origin;
-  const url = window.location.pathname;
+  const url = window.location.origin;
+  // const url = window.location.pathname;
 
   if (email == auth.email && password == auth.password) {
-    window.location.href = `${url}/banking.html`;
+    window.location.href = `${
+      url.includes("hasanthebest.github.io")
+        ? url + "/bank-deposit-withdraw"
+        : "/banking.html"
+    }`;
   } else {
     alert(
       "Password or email doesn't match.\nFollowing are the valid credentials \nEmail: taka@bank.com\nPassword: taka_dey"
